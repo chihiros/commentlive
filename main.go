9016c01bf12ea0ea0153ffd6dab074f0372b67a9
chihiros <szrkw2ui2a@gmail.com>
2022-12-26 01:41:11 +0900

    update: importの更新

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"embed"
 
+	"github.com/go-gl/glfw/v3.2/glfw"
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
