a6eedf0052af0c175ac3b37131d0ee5efed81bc5
chihiros <szrkw2ui2a@gmail.com>
2023-05-01 00:21:00 +0900

    del: コメントを削除

--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,36 @@
 import { app, BrowserWindow, screen } from "electron";
+import * as path from "path";
 
 function createWindow() {
   // Windowのサイズを取得する
   const { width, height } = screen.getPrimaryDisplay().workAreaSize;
-  console.log(width, height);
+  // console.log(width, height);
 
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: width,
-    height: height,
-    // width: 800,
-    // height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-    },
+    // width: width,
+    // height: height,
+    width: 800,
+    height: 600,
+    transparent: true,
+
+    // webPreferences: {
+    //   preload: path.join(__dirname, "preload.js"),
+    // },
   });
+
+  mainWindow.loadFile(path.join(__dirname, "../index.html"));
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow();
 
   app.on("activate", function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
-
-// In this file you can include the rest of your app"s specific main process
-// code. You can also put them in separate files and require them here.
