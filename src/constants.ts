a17c006ff902359415bd0cf8940db9bc285c8228
chihiros <szrkw2ui2a@gmail.com>
2023-05-01 22:49:53 +0900

    fix: 関数に変更

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,10 @@ import { screen } from "electron";
 const HOSTNAME = "http://localhost:8080";
 export { HOSTNAME };
 
-// 画面サイズをAPIから取得
-const { width, height } = screen.getPrimaryDisplay().workAreaSize;
-const { SCREEN_WIDTH, SCREEN_HEIGHT } = { SCREEN_WIDTH: width, SCREEN_HEIGHT: height };
-export { SCREEN_WIDTH, SCREEN_HEIGHT };
+/**
+ * V8のAPIを使い画面サイズを取得します
+ */
+export const GetScreenSize: () => { SCREEN_WIDTH: number, SCREEN_HEIGHT: number } = () => {
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+  return { SCREEN_WIDTH: width, SCREEN_HEIGHT: height };
+}
