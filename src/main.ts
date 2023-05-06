c0bc3138a958859d11b3e52e104e4c81371c651a
chihiros <szrkw2ui2a@gmail.com>
2023-05-07 00:14:22 +0900

    fix: cacheの位置を変更

--- src/main.ts
+++ src/main.ts
@@ -50,113 +50,119 @@ app.whenReady().then(() => {
       win.setIgnoreMouseEvents(true);
       console.log("handlePrompt - then");
       console.log(e);
+
+      let tray: Tray;
+      if (is_windows) tray = new Tray(`${__dirname}/images/icon.ico`);
+      else if (is_mac) tray = new Tray(`${__dirname}/images/icon.png`);
+
+      // const screens = screen.getAllDisplays();
+
+      // let data_append: Electron.MenuItemConstructorOptions;
+      // data_append.label = '表示ディスプレイ選択';
+
+      // const submenu: Electron.MenuItemConstructorOptions[] = [];
+      // for (const sc of screens) {
+      //   submenu.push({
+      //     label: `Display-${sc.id} [${sc.bounds.x}, ${sc.bounds.y}] ${sc.bounds.width}x${sc.bounds.height}`,
+      //     type: 'radio',
+      //     // x: sc.workArea.x,
+      //     // y: sc.workArea.y,
+      //     // w: sc.workArea.width,
+      //     // h: sc.workArea.height,
+      //     click: function (item: any) {
+      //       console.log(item);
+      //       win.setPosition(item.x, item.y, true);
+      //       win.setSize(item.w, item.h, true);
+      //       console.log(item.x, item.y, item.w, item.h);
+      //     }
+      //   });
+      // }
+      // data_append.submenu = submenu;
+      // const cMenu = contextMenu(win);
+      const cMenu = Menu.buildFromTemplate([
+        {
+          label: "投稿ページを開く", click: async () => {
+            await shell.openExternal(HOSTNAME + '/?room=' + GetRoomName());
+          }
+        },
+        {
+          label: '投稿ページURLをコピー',
+          click() {
+            clipboard.writeText(HOSTNAME + '/?room=' + GetRoomName());
+            console.log(HOSTNAME + '/?room=' + encodeURI(GetRoomName()));
+          }
+        },
+        {
+          type: 'separator',
+        },
+        {
+          label: "QR Code表示",
+          submenu: [
+            {
+              label: '非表示', type: 'radio',
+              click(item, focusedWindow) {
+                console.log(item, focusedWindow);
+                win.webContents.executeJavaScript(`toggleQR(${item.checked}, "none", "${GetRoomName()}");`, true)
+                  .catch(console.error);
+              }
+            },
+            {
+              label: 'QR Code [CENTER]', type: 'radio',
+              click(item, focusedWindow) {
+                console.log(item, focusedWindow);
+                win.webContents.executeJavaScript(`toggleQR(${item.checked}, "center", "${GetRoomName()}");`, true)
+                  .catch(console.error);
+              }
+            },
+            {
+              label: 'QR Code [TOP RIGHT]', type: 'radio', checked: true,
+              click(item, focusedWindow) {
+                console.log(item, focusedWindow);
+                win.webContents.executeJavaScript(`toggleQR(${item.checked}, "top_right", "${GetRoomName()}");`, true)
+                  .catch(console.error);
+              }
+            },
+          ]
+        },
+
+        {
+          label: '投稿制限解除', type: 'checkbox',
+          click(item) {
+            win.webContents.executeJavaScript(`toggleCommentControl(${item.checked});`, true)
+              .catch(console.error);
+          }
+        },
+
+        {
+          label: 'Mute sound', type: 'checkbox',
+          click() {
+            win.webContents.executeJavaScript(`toggleSoundMute();`, true)
+              .catch(console.error);
+          }
+        },
+        {
+          type: 'separator',
+        },
+        { label: 'Quit Commentable-Viewer', role: 'quit' },
+      ]);
+      // cMenu.insert(3, new MenuItem(data_append));
+
+      tray.setToolTip('commentable-viewer')
+
+      tray.setContextMenu(cMenu)
+      //クリック時の操作を設定
+      tray.on('click', () => {
+        // メニューを表示
+        tray.popUpContextMenu(cMenu)
+      })
+
+      console.log("GetRoomName(): ", GetRoomName());
+
+
+      win.webContents.executeJavaScript(`startSocketConnection("${GetRoomName()}");`, true)
+        .catch(console.error);
     })
     .catch(console.error);
-
-  let tray: Tray;
-  if (is_windows) tray = new Tray(`${__dirname}/images/icon.ico`);
-  else if (is_mac) tray = new Tray(`${__dirname}/images/icon.png`);
-
-  // const screens = screen.getAllDisplays();
-
-  // let data_append: Electron.MenuItemConstructorOptions;
-  // data_append.label = '表示ディスプレイ選択';
-
-  // const submenu: Electron.MenuItemConstructorOptions[] = [];
-  // for (const sc of screens) {
-  //   submenu.push({
-  //     label: `Display-${sc.id} [${sc.bounds.x}, ${sc.bounds.y}] ${sc.bounds.width}x${sc.bounds.height}`,
-  //     type: 'radio',
-  //     // x: sc.workArea.x,
-  //     // y: sc.workArea.y,
-  //     // w: sc.workArea.width,
-  //     // h: sc.workArea.height,
-  //     click: function (item: any) {
-  //       console.log(item);
-  //       win.setPosition(item.x, item.y, true);
-  //       win.setSize(item.w, item.h, true);
-  //       console.log(item.x, item.y, item.w, item.h);
-  //     }
-  //   });
-  // }
-  // data_append.submenu = submenu;
-  // const cMenu = contextMenu(win);
-  const cMenu = Menu.buildFromTemplate([
-    {
-      label: "投稿ページを開く", click: async () => {
-        await shell.openExternal(HOSTNAME + '/?room=' + GetRoomName());
-      }
-    },
-    {
-      label: '投稿ページURLをコピー',
-      click() {
-        clipboard.writeText(HOSTNAME + '/?room=' + GetRoomName());
-        console.log(HOSTNAME + '/?room=' + encodeURI(GetRoomName()));
-      }
-    },
-    {
-      type: 'separator',
-    },
-    {
-      label: "QR Code表示",
-      submenu: [
-        {
-          label: '非表示', type: 'radio',
-          click(item, focusedWindow) {
-            console.log(item, focusedWindow);
-            win.webContents.executeJavaScript(`toggleQR(${item.checked}, "none", "${GetRoomName()}");`, true)
-              .catch(console.error);
-          }
-        },
-        {
-          label: 'QR Code [CENTER]', type: 'radio',
-          click(item, focusedWindow) {
-            console.log(item, focusedWindow);
-            win.webContents.executeJavaScript(`toggleQR(${item.checked}, "center", "${GetRoomName()}");`, true)
-              .catch(console.error);
-          }
-        },
-        {
-          label: 'QR Code [TOP RIGHT]', type: 'radio', checked: true,
-          click(item, focusedWindow) {
-            console.log(item, focusedWindow);
-            win.webContents.executeJavaScript(`toggleQR(${item.checked}, "top_right", "${GetRoomName()}");`, true)
-              .catch(console.error);
-          }
-        },
-      ]
-    },
-
-    {
-      label: '投稿制限解除', type: 'checkbox',
-      click(item) {
-        win.webContents.executeJavaScript(`toggleCommentControl(${item.checked});`, true)
-          .catch(console.error);
-      }
-    },
-
-    {
-      label: 'Mute sound', type: 'checkbox',
-      click() {
-        win.webContents.executeJavaScript(`toggleSoundMute();`, true)
-          .catch(console.error);
-      }
-    },
-    {
-      type: 'separator',
-    },
-    { label: 'Quit Commentable-Viewer', role: 'quit' },
-  ]);
-  // cMenu.insert(3, new MenuItem(data_append));
-
-  tray.setToolTip('commentable-viewer')
-
-  tray.setContextMenu(cMenu)
-  //クリック時の操作を設定
-  tray.on('click', () => {
-    // メニューを表示
-    tray.popUpContextMenu(cMenu)
-  })
 
   win.webContents.on('did-finish-load', () => {
     win.show();
